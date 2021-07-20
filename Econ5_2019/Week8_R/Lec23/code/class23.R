e598e2c8b3b2c9c390ecbbe015ee53f4c6295524
Arushi Kaushik <arushikaushik1990@gmail.com>
2021-07-19 21:15:26 -0400

    Create class23.R

---
+++ Econ5_2019/Week8_R/Lec23/code/class23.R
@@ -0,0 +1,109 @@
+# Econ 5/Poli 5D, Class 23
+# In-class Activity
+# 
+# Instructions: Below is some partially completed code. Using the comments as a 
+# guide, finish the code. Remember to use help() if you cannot remember how a 
+# function works, for example, help("mean"). Feel free to ask questions and
+# help your classmates. 
+################################################################################
+
+# Start-up commands
+rm(list = ls())
+
+################################################################################
+# Pre Class Exercise: setup for regression in R
+################################################################################
+
+# Change directory and check
+setwd("")
+getwd()
+
+# Load data
+nba <- read.csv("nba.csv")
+florida <- read.csv("florida.csv")
+face <- read.csv("face.csv")
+
+# Make a scatter plot of height vs blocks using the nba data frame.
+plot(__, __, 
+     __ = "Height", 
+     __ = "Blocks", 
+     __ = 16, 
+     __ = "Height of NBA Players and Blocks")
+
+
+################################################################################
+# In Class Activity
+################################################################################
+
+## First class example: NBA
+
+# What is the correlation between height and blocks?
+
+
+# Fit a regression line of the form:
+#   blocks = b0 + b1 * height
+fit <- 
+
+# Check the results
+
+
+# Now add the best fit line to the plot. 
+
+
+# Now add the players' names to the plot. 
+
+
+#---------------------------------------------------------------------
+## Second example: florida
+head(florida)
+
+# Make a scatterplot of florida$Perot96 vs florida$Buchanan00
+#     and label appropriately.
+plot(florida$Perot96, florida$Buchanan00, 
+     __ = "Perot's votes in 1996",
+     __ = "Buchanan's votes in 2000", 
+     __ = "Third Party Vote in Florida, 1996 & 2000",
+     __ = 16)
+
+# Fit a best fit line following the model:
+#     florida$Buchanan00 = b0 + b1 * florida$Perot96
+fit2 <- 
+
+# Add the trendline to your plot.
+
+
+# Which county is furthest from the trendline?
+#     hint: use max(resid()) to find the greatest residual
+florida$county[]
+
+#---------------------------------------------------------------------
+## Third example: face experiment
+head(face)
+
+# Define vote share for Dems and Reps
+face$d.share <- face$d.votes/(face$d.votes + face$r.votes)
+face$r.share <- face$r.votes/(face$d.votes + face$r.votes)
+face$diff.share <- face$d.share - face$r.share
+
+# Plot vote margin in favor of the Democratic candidate as a function of her competence score
+face$w.party <- as.character(face$w.party) # change class of winning party letter to character from factor
+plot(face$d.comp, face$diff.share, 
+     pch = face$w.party, 
+     col = ifelse(face$w.party == "R", "red", "blue"), 
+     __ = c(0,1), 
+     __ = c(-1,1),
+     __ = "Competence scores for Democrats",
+     __ = "Democratic margin in vote share",
+     __ = "Facial Competence in Vote Share")
+
+# Regress the model diff.share = beta0 + beta1 * d.comp
+fit3 <- 
+
+# Add a trendline to your plot
+
+
+# Interpret the coefficients. Is there a causal effect of appearance on outcome?
+  
+  
+  
+  
